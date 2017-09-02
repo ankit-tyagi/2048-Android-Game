9b37a52676ab901e50cc272d212e07ff56e84529
Ankit Tyagi <ankittyaagi@gmail.com>
2017-09-02 16:22:48 +0530

    add style script

---
+++ Assets/Scripts/TileStyleHolder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class TileStyle {
+	public int number;
+	public Color32 TileColor;
+	public Color32 TextColor;
+}
+
+public class TileStyleHolder : MonoBehaviour {
+
+	public static TileStyleHolder Instance;
+	public TileStyle[] TileStyles;	
+
+	void Awake(){
+		Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
