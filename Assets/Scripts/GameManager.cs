f7ec42b8b848732f412a02ecd3c9d0f78300f784
Ankit Tyagi <ankittyaagi@gmail.com>
2017-09-02 16:11:58 +0530

    scripts working along with keyboard inputs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Move(MoveDirection md) {
+		Debug.Log (md.ToString () + "move.");
+	}
+
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MoveDirection{
+	Left, Right, Up, Down
+}
+
+
+public class InputManager : MonoBehaviour {
+
+	public GameManager gm;
+
+	void Awake(){
+		gm = GameObject.FindObjectOfType<GameManager> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			//Right move
+			gm.Move(MoveDirection.Right);
+		}	else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			//left move
+			gm.Move(MoveDirection.Left);
+		} 	else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			//Up move
+			gm.Move(MoveDirection.Up);
+		} 	else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			//Down move
+			gm.Move(MoveDirection.Down);
+		}
+	}
+}
