c68b48a4cc3213fd9fc8416604fe8261958aceaf
Ankit Tyagi <ankittyaagi@gmail.com>
2017-09-02 16:48:01 +0530

    give tile value and make tile script

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour {
+
+	private Text TileText;
+	private Image TileImage;
+
+	void Awake(){
+		TileText = GetComponentInChildren<Text> ();
+		TileImage = transform.Find ("Number Cell").GetComponent<Image> ();
+	}
+
+	void ApplyStyleFromHolder(int index){
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
